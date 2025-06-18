9a8a638a968ff8964fb2b011b29fff49e377f64a
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2025-06-18 14:46:23 +0100

    Add collision code

--- src/arc_puzzle_generator/collisions.py
+++ src/arc_puzzle_generator/collisions.py
@@ -4,6 +4,14 @@ from arc_puzzle_generator.physics import Direction
 
 
 def collision_neighbourhood(point: np.ndarray, direction: Direction) -> np.ndarray:
+    """
+    Determines the neighbourhood of a point based on a direction.
+    :param point: The point to determine the neighbourhood for.
+    :param direction: The direction to determine the neighbourhood into.
+    :return: A 2D array of neighbourhood coordinates.
+    """
+
+    # TODO: Implement multiple step size diagonal collisions
     y_min = point[:, 1].min()
     y_max = point[:, 1].max()
     x_min = point[:, 0].min()
@@ -29,6 +37,22 @@ def collision_neighbourhood(point: np.ndarray, direction: Direction) -> np.ndarr
             return np.array([
                 [x_max + 1, y]
                 for y in range(y_min, y_max + 1)
+            ])
+        case "top_left":
+            return np.array([
+                (x_min - 1, y_min - 1), (x_min - 1, y_min), (x_min, y_min - 1)
+            ])
+        case "top_right":
+            return np.array([
+                (x_min - 1, y_max), (x_min - 1, y_max + 1), (x_min, y_max + 1)
+            ])
+        case "bottom_left":
+            return np.array([
+                (x_min, y_min - 1), (x_min + 1, y_min - 1), (x_min + 1, y_min)
+            ])
+        case "bottom_right":
+            return np.array([
+                (x_max, y_max + 1), (x_max + 1, y_max), (x_max + 1, y_max + 1)
             ])
 
     raise ValueError(f"Invalid direction: {direction}")
--- tests/test_collisions.py
+++ tests/test_collisions.py
@@ -105,3 +105,65 @@ class CollisionTest(TestCase):
             [[6, 3], [6, 4]],
             collision_neighbourhood(step_three, "down")
         ))
+
+    def test_collision_direction_top_left(self):
+        step = np.array([[5, 3]])
+
+        self.assertTrue(np.array_equal(
+            [[4, 2], [4, 3], [5, 2]],
+            collision_neighbourhood(step, "top_left")
+        ))
+
+        step_two = np.array([
+            [5, 3],
+            [6, 4]
+        ])
+
+        self.assertTrue(np.array_equal(
+            [[4, 2], [4, 3], [5, 2]],
+            collision_neighbourhood(step_two, "top_left")
+        ))
+
+    def test_collision_direction_top_right(self):
+        step = np.array([[5, 3]])
+
+        self.assertTrue(np.array_equal(
+            [[4, 3], [4, 4], [5, 4]],
+            collision_neighbourhood(step, "top_right")
+        ))
+
+        step_two = np.array([
+            [5, 3],
+            [6, 2]
+        ])
+        self.assertTrue(np.array_equal(
+            [[4, 3], [4, 4], [5, 4]],
+            collision_neighbourhood(step_two, "top_right")
+        ))
+
+        # step_three = np.array([
+        #    [5, 3], [6, 4]
+        # ])
+        # step_three_neighbourhood = np.array([
+        #    [4, 3], [4, 4], [5, 4],
+        #    [5, 5], [6, 5]
+        # ])
+
+        # self.assertTrue(np.array_equal(
+        #    step_three_neighbourhood,
+        #    collision_neighbourhood(step_three, "top_right")
+        # ))
+
+    def test_collision_direction_bottom_left(self):
+        step = np.array([[5, 3]])
+        self.assertTrue(np.array_equal(
+            [[5, 2], [6, 2], [6, 3]],
+            collision_neighbourhood(step, "bottom_left")
+        ))
+
+    def test_collision_direction_bottom_right(self):
+        step = np.array([[5, 3]])
+        self.assertTrue(np.array_equal(
+            [[5, 4], [6, 3], [6, 4]],
+            collision_neighbourhood(step, "bottom_right")
+        ))
