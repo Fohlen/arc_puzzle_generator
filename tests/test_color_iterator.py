10df28c5902ce26b9c51e684c0016e955be39f35
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2025-06-19 18:16:49 +0100

    More tests

--- tests/test_color_iterator.py
+++ tests/test_color_iterator.py
@@ -8,5 +8,17 @@ class TestColorIterator(TestCase):
         color_sequence = [(4, 2), (3, 3)]
         color_iterator = ColorIterator(color_sequence)
 
-        colors = [color_iterator.__next__() for _ in range(10)]
+        colors = [next(color_iterator) for _ in range(10)]
         self.assertEqual(colors, [4, 0, 4, 3, 4, 0, 4, 0, 4, 3])
+
+    def test_color_iterator_complex(self):
+        color_sequence = [(4, 2), (3, 3), (2, 1)]
+        color_iterator = ColorIterator(color_sequence)
+        colors = [next(color_iterator) for _ in range(10)]
+        self.assertEqual(colors, [4, 2, 4, 3, 4, 2, 4, 2, 4, 3])
+
+    def test_color_iterator_empty(self):
+        color_sequence = []
+        color_iterator = ColorIterator(color_sequence, background_color=5)
+        color = next(color_iterator)
+        self.assertEqual(color, 5)
