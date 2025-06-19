e7eca0e736bbe870397e9a34411061f156ba236a
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2025-06-19 17:43:08 +0100

    Support border color in both rules

--- src/arc_puzzle_generator/collision_rules/BoundingBoxRule.py
+++ src/arc_puzzle_generator/collision_rules/BoundingBoxRule.py
@@ -1,10 +1,10 @@
-from itertools import cycle
+from itertools import cycle, chain
 from typing import Optional, Iterable
 
 import numpy as np
 
 from arc_puzzle_generator.collisions import CollisionResult, CollisionRule
-from arc_puzzle_generator.physics import Direction, contained, line_axis, DirectionRule
+from arc_puzzle_generator.physics import Direction, contained, line_axis, DirectionRule, direction_to_unit_vector
 
 
 class BoundingBoxRule(CollisionRule):
@@ -12,9 +12,15 @@ class BoundingBoxRule(CollisionRule):
     Uses a bounding box to determine collisions.
     """
 
-    def __init__(self, bounding_box: np.ndarray, direction_rule: DirectionRule) -> None:
+    def __init__(
+            self,
+            bounding_box: np.ndarray,
+            direction_rule: DirectionRule,
+            border_color: Optional[int] = None,
+    ) -> None:
         self.bounding_box = bounding_box
         self.direction_rule = direction_rule
+        self.border_color = border_color
 
     def __call__(
             self,
@@ -30,6 +36,10 @@ class BoundingBoxRule(CollisionRule):
             block = neighbourhood[np.where(colliding_blocks)][0]
             current_color: int = output_grid[block[0], block[1]]
             axis = line_axis(neighbourhood[np.where(colliding_blocks)])
+
+            if self.border_color is not None:
+                border_step = step + direction_to_unit_vector(direction)
+                return False, chain([self.border_color], colors), self.direction_rule(direction, axis), border_step
 
             return False, cycle([current_color]), self.direction_rule(direction, axis), None
         else:
