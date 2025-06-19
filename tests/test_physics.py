55cbc6a6281ec52dafa609b79f0af388b72e0389
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2025-06-19 18:41:11 +0100

    More tests

--- tests/test_physics.py
+++ tests/test_physics.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-from arc_puzzle_generator.physics import direction_to_unit_vector
+from arc_puzzle_generator.physics import direction_to_unit_vector, contained
 
 
 class PhysicsTestCase(unittest.TestCase):
@@ -20,3 +20,30 @@ class PhysicsTestCase(unittest.TestCase):
 
         bottom_right = direction_to_unit_vector("bottom_right") + arr
         self.assertTrue(np.array_equal(bottom_right, np.array([5, 5])))
+
+    def test_contained(self):
+        points = np.array([
+            [0, 0],
+            [1, 1],
+            [10, 0]
+        ])
+
+        box = np.array([[
+            [5, 0], [0, 0],
+            [0, 5], [5, 5]
+        ]])
+
+        collisions = contained(points, box)
+        self.assertTrue(np.any(collisions))
+        self.assertFalse(np.all(collisions))
+
+    def test_not_contained(self):
+        point = np.array([[15, 20]])
+
+        box = np.array([[
+            [5, 0], [0, 0],
+            [0, 5], [5, 5]
+        ]])
+
+        collisions = contained(point, box)
+        self.assertFalse(np.any(collisions))
