129594f79e73480103e5461ff17ca28eb1df2864
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2025-07-15 08:56:45 +0200

    Revert "Cleanup"
    This reverts commit da12c8b9cdf3804dfcaf5753c74faff99432443b.

---
+++ tests/test_selection.py
@@ -0,0 +1,39 @@
+from unittest import TestCase
+
+from abm.neighbourhood import moore_neighbours
+from abm.selection import top_selector, bottom_selector, left_selector, right_selector, resolve_point_set_selectors
+
+
+class SelectorsTestCase(TestCase):
+    def setUp(self) -> None:
+        self.point = (1, 1)
+        self.neighbourhood = moore_neighbours(self.point)
+
+    def test_top_selector(self):
+        expected = {(0, 0), (0, 1), (0, 2)}
+        self.assertEqual(expected, top_selector(self.point, self.neighbourhood))
+
+    def test_bottom_selector(self):
+        expected = {(2, 0), (2, 1), (2, 2)}
+        self.assertEqual(expected, bottom_selector(self.point, self.neighbourhood))
+
+    def test_left_selector(self):
+        expected = {(0, 0), (1, 0), (2, 0)}
+        self.assertEqual(expected, left_selector(self.point, self.neighbourhood))
+
+    def test_right_selector(self):
+        expected = {(0, 2), (1, 2), (2, 2)}
+        self.assertEqual(expected, right_selector(self.point, self.neighbourhood))
+
+    def test_combined_selectors(self):
+        combined = top_selector(self.point, self.neighbourhood) | bottom_selector(self.point, self.neighbourhood) | \
+            left_selector(self.point, self.neighbourhood) | right_selector(self.point, self.neighbourhood)
+        self.assertEqual(self.neighbourhood, combined)
+
+    def test_resolve_point_set_selectors(self):
+        expected_top = {(0, 0), (0, 1), (0, 2)}
+        self.assertEqual(expected_top, resolve_point_set_selectors({self.point}, self.neighbourhood, top_selector))
+
+        expected_bottom =  {(2, 0), (2, 1), (2, 2)}
+        self.assertEqual(expected_bottom, resolve_point_set_selectors({self.point}, self.neighbourhood, bottom_selector))
+
