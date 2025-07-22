4403fc8b2e9af51a99cf10a9b2a879d3a3be5b58
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2025-07-22 10:23:08 +0200

    Add collision rule test case

--- tests/test_rule.py
+++ tests/test_rule.py
@@ -1,8 +1,9 @@
 from unittest import TestCase
 
+from arc_puzzle_generator.direction import orthogonal_direction
 from arc_puzzle_generator.geometry import PointSet
 from arc_puzzle_generator.physics import Direction
-from arc_puzzle_generator.rule import identity_rule, DirectionRule
+from arc_puzzle_generator.rule import identity_rule, DirectionRule, CollisionDirectionRule
 from arc_puzzle_generator.state import AgentState
 
 
@@ -50,3 +51,29 @@ class RuleTest(TestCase):
         rule = DirectionRule(dummy_direction_rule)
         result = rule(states, colors, collision)
         self.assertIsNone(result)
+
+    def test_collision_direction_rule_no_collision(self):
+        states = [AgentState(PointSet([(0, 0)]), "left", 1, 1)]
+        colors = iter([1, 2])
+        collision = PointSet()  # No collision
+
+        rule = CollisionDirectionRule(direction_rule=orthogonal_direction)
+        result = rule(states, colors, collision, {})
+        self.assertIsNone(result)
+
+    def test_collision_direction_rule_with_collision(self):
+        states = [AgentState(PointSet([(0, 0)]), "bottom_right", 1, 1)]
+        colors = iter([1, 2])
+        collision = PointSet([(1, 1)])  # Collision present
+
+        rule = CollisionDirectionRule(orthogonal_direction)
+        result = rule(states, colors, collision, {
+            (1, 1): AgentState(PointSet([(1, 1)]), "none", 2, 0)
+        })
+
+        self.assertIsNotNone(result)
+        new_state, new_colors = result
+        self.assertEqual("top_right", new_state.direction)
+        self.assertIn((-1, 1), new_state.position)
+        self.assertEqual(0, new_state.charge)
+        self.assertEqual(1, new_state.color)
