47d156c32e3414915bb19738b980717e0a0b32fe
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2025-07-25 09:29:45 +0200

    Add commit flag to agent state

--- src/arc_puzzle_generator/playground.py
+++ src/arc_puzzle_generator/playground.py
@@ -114,11 +114,13 @@ class Playground(Iterator[np.ndarray], Iterable[np.ndarray]):
         previous_position = agent.position
 
         for step in agent.steps(position_intersect, position_intersect_mapping):
-            pos, _, color, charge = step
+            pos, direction, color, charge, commit = step
             if charge > 0 or charge == -1:
                 position = np.array(list(pos))
                 logger.debug("Position: %s, Color: %s", pos, color)
-                self.output_grid[position[:, 0], position[:, 1]] = color
+
+                if commit:
+                    self.output_grid[position[:, 0], position[:, 1]] = color
 
                 # Fill the previous position with the backfill color if specified
                 if self.backfill_color is not None and previous_position is not pos:
