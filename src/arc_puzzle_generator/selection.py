ef05d99b9db366c66c4dc5911b258c72887481e7
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2025-08-05 19:16:08 +0200

    Simplify selection logic

--- src/arc_puzzle_generator/selection.py
+++ src/arc_puzzle_generator/selection.py
@@ -1,68 +1,7 @@
-from typing import Protocol
+from typing import Protocol, cast
 
 from arc_puzzle_generator.geometry import Point, PointSet, Direction
 from arc_puzzle_generator.physics import direction_to_unit_vector
-
-
-class Selector(Protocol):
-    """
-    A selector is a callable that takes a point and a point set and returns a set of points.
-    Selectors are chainable operators, e.g:
-    ```
-    p = (1, 1)
-    neighbourhood = moore_neighbours(p)
-    top_left = left_selector(p, top_selector(p, neighbourhood))
-    ```
-    """
-
-    def __call__(self, point: Point, point_set: PointSet) -> PointSet:
-        pass
-
-
-def up_selector(point: Point, point_set: PointSet) -> PointSet:
-    """
-    Selects points that are above the given point in the point set.
-
-    :param point: The reference point.
-    :param point_set: The set of points to select from.
-    :return: A set of points that are above the reference point.
-    """
-
-    return PointSet((x, y) for (x, y) in point_set if x < point[0] and y == point[1])
-
-
-def down_selector(point: Point, point_set: PointSet) -> PointSet:
-    """
-    Selects points that are below the given point in the point set.
-
-    :param point: The reference point.
-    :param point_set: The set of points to select from.
-    :return: A set of points that are below the reference point.
-    """
-
-    return PointSet((x, y) for (x, y) in point_set if x > point[0] and y == point[1])
-
-
-def left_selector(point: Point, point_set: PointSet) -> PointSet:
-    """
-    Selects points that are to the left of the given point in the point set.
-    :param point: The reference point.
-    :param point_set: The set of points to select from.
-    :return: A set of points that are to the left of the reference point.
-    """
-
-    return PointSet((x, y) for (x, y) in point_set if y < point[1] and x == point[0])
-
-
-def right_selector(point: Point, point_set: PointSet) -> PointSet:
-    """
-    Selects points that are to the right of the given point in the point set.
-    :param point: The reference point.
-    :param point_set: The set of points to select from.
-    :return: A set of points that are to the right of the reference point.
-    """
-
-    return PointSet((x, y) for (x, y) in point_set if y > point[1] and x == point[0])
 
 
 def direction_selector(point: Point, point_set: PointSet, direction: Direction) -> PointSet:
@@ -74,19 +13,7 @@ def direction_selector(point: Point, point_set: PointSet, direction: Direction) 
     :return: A set of points in the specified direction from the reference point.
     """
 
-    return PointSet([point]).shift(direction_to_unit_vector(direction)) & point_set
-
-
-def resolve_point_set_selectors(point_set: PointSet, neighbourhood: PointSet, selector: Selector) -> PointSet:
-    """
-    Resolves a point set using a selector function.
-    :param point_set: The point set to resolve for.
-    :param neighbourhood: The neighbourhood of the point set to use for selection.
-    :param selector: The selector function to use.
-    :return: A set of points selected from the point set based on the selector.
-    """
-
-    return PointSet(set.union(*(selector(point, neighbourhood) for point in point_set)) - point_set)
+    return cast(PointSet, PointSet([point]).shift(direction_to_unit_vector(direction)) & point_set)
 
 
 def resolve_point_set_selectors_with_direction(
@@ -100,14 +27,4 @@ def resolve_point_set_selectors_with_direction(
     :return: A set of points selected from the point set based on the direction.
     """
 
-    match direction:
-        case "up":
-            return resolve_point_set_selectors(point_set, neighbourhood, up_selector)
-        case "down":
-            return resolve_point_set_selectors(point_set, neighbourhood, down_selector)
-        case "left":
-            return resolve_point_set_selectors(point_set, neighbourhood, left_selector)
-        case "right":
-            return resolve_point_set_selectors(point_set, neighbourhood, right_selector)
-        case _:
-            return PointSet(set.union(*[direction_selector(point, neighbourhood, direction) for point in point_set]))
+    return PointSet(set.union(*[direction_selector(point, neighbourhood, direction) for point in point_set]))
--- tests/test_selection.py
+++ tests/test_selection.py
@@ -1,8 +1,7 @@
 from unittest import TestCase
 
-from arc_puzzle_generator.neighbourhood import moore_neighbours, von_neumann_neighbours
-from arc_puzzle_generator.selection import up_selector, down_selector, left_selector, right_selector, \
-    resolve_point_set_selectors, resolve_point_set_selectors_with_direction
+from arc_puzzle_generator.neighbourhood import moore_neighbours
+from arc_puzzle_generator.selection import resolve_point_set_selectors_with_direction
 
 
 class SelectorsTestCase(TestCase):
@@ -12,34 +11,35 @@ class SelectorsTestCase(TestCase):
 
     def test_top_selector(self):
         expected = {(0, 1)}
-        self.assertEqual(expected, up_selector(self.point, self.neighbourhood))
+        self.assertEqual(
+            expected,
+            resolve_point_set_selectors_with_direction({self.point}, self.neighbourhood, "up")
+        )
 
     def test_bottom_selector(self):
         expected = {(2, 1)}
-        self.assertEqual(expected, down_selector(self.point, self.neighbourhood))
+        self.assertEqual(
+            expected,
+            resolve_point_set_selectors_with_direction({self.point}, self.neighbourhood, "down")
+        )
 
     def test_left_selector(self):
         expected = {(1, 0)}
-        self.assertEqual(expected, left_selector(self.point, self.neighbourhood))
+        self.assertEqual(
+            expected,
+            resolve_point_set_selectors_with_direction({self.point}, self.neighbourhood, "left")
+        )
 
     def test_right_selector(self):
         expected = {(1, 2)}
-        self.assertEqual(expected, right_selector(self.point, self.neighbourhood))
-
-    def test_combined_selectors(self):
-        combined = up_selector(self.point, self.neighbourhood) | down_selector(self.point, self.neighbourhood) | \
-                   left_selector(self.point, self.neighbourhood) | right_selector(self.point, self.neighbourhood)
-        neighbours = von_neumann_neighbours(self.point)
-        self.assertEqual(neighbours, combined)
-
-    def test_resolve_point_set_selectors(self):
-        expected_top = {(0, 1), }
-        self.assertEqual(expected_top, resolve_point_set_selectors({self.point}, self.neighbourhood, up_selector))
-
-        expected_bottom = {(2, 1)}
-        self.assertEqual(expected_bottom, resolve_point_set_selectors({self.point}, self.neighbourhood, down_selector))
+        self.assertEqual(
+            expected,
+            resolve_point_set_selectors_with_direction({self.point}, self.neighbourhood, "right")
+        )
 
     def test_direction_selection(self):
         expected_bottom_right = {(2, 2)}
-        self.assertEqual(expected_bottom_right,
-                         resolve_point_set_selectors_with_direction({self.point}, self.neighbourhood, "bottom_right"))
+        self.assertEqual(
+            expected_bottom_right,
+            resolve_point_set_selectors_with_direction({self.point}, self.neighbourhood, "bottom_right")
+        )
