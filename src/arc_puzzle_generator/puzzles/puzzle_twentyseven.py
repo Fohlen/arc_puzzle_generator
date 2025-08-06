63cb0d12a608401d88f1e29ca34311a48bc63576
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2025-08-06 11:23:20 +0200

    Add test case for puzzle 27

---
+++ src/arc_puzzle_generator/puzzles/puzzle_twentyseven.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+from arc_puzzle_generator.playground import Playground
+
+
+def puzzle_twentyseven(input_grid: np.ndarray) -> Playground:
+    """
+    Generates a playground for puzzle 27 based on the input grid.
+    Puzzle 27 is a simple puzzle where agents drop down, and new agents are spawned left and right of the existing agents.
+    :param input_grid: The input grid representing the puzzle layout.
+    :return: The generated playground for puzzle 27.
+    """
+    pass
---
+++ tests/test_puzzle_twentyseven.py
@@ -0,0 +1,53 @@
+from unittest import TestCase
+
+import numpy as np
+
+from arc_puzzle_generator.puzzles.puzzle_twentyseven import puzzle_twentyseven
+from arc_puzzle_generator.utils.data_loader import load_puzzle
+from tests.utils import test_dir
+
+
+class PuzzleTwentySevenTestCase(TestCase):
+    def setUp(self):
+        file_path = test_dir / "data" / "36a08778.json"
+        self.puzzle = load_puzzle(file_path)
+
+    def test_gest_generate_36a08778(self):
+        playground = puzzle_twentyseven(self.puzzle.train[0].input)
+        *_, output_grid = playground
+        self.assertTrue(np.array_equal(output_grid, self.puzzle.train[0].output))
+
+    def test_generate_36a08778_second(self):
+        playground = puzzle_twentyseven(self.puzzle.train[1].input)
+        *_, output_grid = playground
+        self.assertTrue(np.array_equal(output_grid, self.puzzle.train[1].output))
+
+    def test_generate_36a08778_third(self):
+        playground = puzzle_twentyseven(self.puzzle.train[2].input)
+        *_, output_grid = playground
+        self.assertTrue(np.array_equal(output_grid, self.puzzle.train[2].output))
+
+    def test_generate_36a08778_fourth(self):
+        playground = puzzle_twentyseven(self.puzzle.train[3].input)
+        *_, output_grid = playground
+        self.assertTrue(np.array_equal(output_grid, self.puzzle.train[3].output))
+
+    def test_generate_36a08778_fifth(self):
+        playground = puzzle_twentyseven(self.puzzle.train[4].input)
+        *_, output_grid = playground
+        self.assertTrue(np.array_equal(output_grid, self.puzzle.train[4].output))
+
+    def test_generate_36a08778_sixth(self):
+        playground = puzzle_twentyseven(self.puzzle.train[5].input)
+        *_, output_grid = playground
+        self.assertTrue(np.array_equal(output_grid, self.puzzle.train[5].output))
+
+    def test_generate_36a08778_prompt(self):
+        playground = puzzle_twentyseven(self.puzzle.test[0].input)
+        *_, output_grid = playground
+        self.assertTrue(np.array_equal(output_grid, self.puzzle.test[0].output))
+
+    def test_generate_36a08778_prompt_second(self):
+        playground = puzzle_twentyseven(self.puzzle.test[1].input)
+        *_, output_grid = playground
+        self.assertTrue(np.array_equal(output_grid, self.puzzle.test[1].output))
