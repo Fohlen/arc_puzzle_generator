afe265849e3af54343872049cfa28564b5d2292b
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2025-08-06 11:34:22 +0200

    First step towards puzzle 27

--- src/arc_puzzle_generator/puzzles/puzzle_twentyseven.py
+++ src/arc_puzzle_generator/puzzles/puzzle_twentyseven.py
@@ -1,6 +1,15 @@
+from itertools import cycle
+
 import numpy as np
 
+from arc_puzzle_generator.agent import Agent
+from arc_puzzle_generator.direction import identity_direction
+from arc_puzzle_generator.neighbourhood import moore_neighbours
 from arc_puzzle_generator.playground import Playground
+from arc_puzzle_generator.rule import identity_rule, RuleNode, OutOfGridRule, TrappedCollisionRule, GravityRule
+from arc_puzzle_generator.topology import all_topology
+from arc_puzzle_generator.utils.entities import find_connected_objects
+from arc_puzzle_generator.utils.grid import unmask
 
 
 def puzzle_twentyseven(input_grid: np.ndarray) -> Playground:
@@ -10,4 +19,44 @@ def puzzle_twentyseven(input_grid: np.ndarray) -> Playground:
     :param input_grid: The input grid representing the puzzle layout.
     :return: The generated playground for puzzle 27.
     """
-    pass
+
+    border_color = 2
+    agent_color = 6
+
+    agents: list[Agent] = []
+    labels, bboxes, num_objects = find_connected_objects(input_grid == border_color)
+    for i in range(1, num_objects + 1):
+        agents.append(Agent(
+            position=unmask(labels == i),
+            direction="none",
+            label="border",
+            node=RuleNode(identity_rule),
+            colors=iter([border_color]),
+            charge=0,
+        ))
+
+    labels_agent, bboxes_agent, num_objects_agent = find_connected_objects(input_grid == agent_color)
+    for i in range(1, num_objects_agent + 1):
+        agents.append(Agent(
+            position=unmask(labels_agent == i),
+            direction="down",
+            label="agent",
+            node=RuleNode(
+                OutOfGridRule(grid_size=input_grid.shape),
+                alternative_node=RuleNode(
+                    TrappedCollisionRule(select_direction=True, direction_rule=identity_direction),
+                    alternative_node=RuleNode(
+                        GravityRule(grid_size=input_grid.shape),
+                    )
+                )
+            ),
+            colors=cycle([agent_color]),
+            charge=-1,
+        ))
+
+    return Playground(
+        output_grid=input_grid,
+        agents=agents,
+        neighbourhood=moore_neighbours,
+        topology=all_topology,
+    )
