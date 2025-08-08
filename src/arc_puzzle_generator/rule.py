ca29b4033976e713472675988fea6428ec733ae2
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2025-08-08 14:53:25 +0200

    Calculate distances only once

--- src/arc_puzzle_generator/rule.py
+++ src/arc_puzzle_generator/rule.py
@@ -465,15 +465,15 @@ class ProximityRule(Rule):
             points: PointSet
     ):
         self.target = target
-        min_distance = min(
-            math.dist(point, target_point) for point in points for target_point in target
-        )
-        max_distance = max(
-            math.dist(point, target_point) for point in points for target_point in target
-        )
+        distances = {
+            (point, target_point): math.dist(point, target_point)
+            for point in points for target_point in target
+        }
+        min_distance = min(distances.values())
+        max_distance = max(distances.values())
         self.proximity_mapping = {
             point: (
-                (min(math.dist(point, target_point) for target_point in target) - min_distance)
+                (min(distances[(point, target_point)] for target_point in target) - min_distance)
                 / (max_distance - min_distance) if max_distance > min_distance else 0.0
             )
             for point in points
