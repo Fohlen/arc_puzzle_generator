b558b51ba6083a5e62ecd6717d0ab32bb740b103
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2025-08-13 11:31:40 +0200

    Register balls

--- src/arc_puzzle_generator/puzzles/puzzle_fourtyfour.py
+++ src/arc_puzzle_generator/puzzles/puzzle_fourtyfour.py
@@ -23,24 +23,34 @@ def puzzle_fourtyfour(input_grid: np.ndarray) -> Playground:
     agent_color = 2
     background_color = 8
 
+    agents: list[Agent] = []
     labels, bbox, num_blocks = find_connected_objects((input_grid == 3) | (input_grid == 1))
 
     for i in range(1, num_blocks + 1):
+        agents.append(Agent(
+            position=unmask(labels == i),
+            direction="none",
+            colors=iter([agent_color]),
+            charge=0,
+            label=f"ball_{i}",
+        ))
         output_grid[labels == i] = background_color
+
+    agents.append(Agent(
+        position=unmask(input_grid == agent_tip_color),
+        direction="up",
+        label="shooter",
+        colors=cycle([agent_color]),
+        node=RuleNode(
+            OutOfGridRule(grid_size=input_grid.shape),
+            alternative_node=RuleNode(
+                DirectionRule(direction_rule=identity_direction)
+            )
+        ),
+        charge=-1
+    ))
 
     return Playground(
         output_grid=output_grid,
-        agents=[Agent(
-            position=unmask(input_grid == agent_tip_color),
-            direction="up",
-            label="shooter",
-            colors=cycle([agent_color]),
-            node=RuleNode(
-                OutOfGridRule(grid_size=input_grid.shape),
-                alternative_node=RuleNode(
-                    DirectionRule(direction_rule=identity_direction)
-                )
-            ),
-            charge=-1
-        )]
+        agents=agents
     )
