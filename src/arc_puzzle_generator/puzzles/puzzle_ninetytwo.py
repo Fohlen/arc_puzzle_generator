e5c3a4701fd84b7107f1872377e31b2272b8b059
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2025-08-20 13:52:52 +0200

    Import test data for puzzle 92

---
+++ src/arc_puzzle_generator/puzzles/puzzle_ninetytwo.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+from arc_puzzle_generator.playground import Playground
+
+
+def puzzle_ninetytwo(input_grid: np.ndarray) -> Playground:
+    """
+    In puzzle 92 one needs to figure out a repeat pattern problem by row / column.
+    :param input_grid: The input grid.
+    :return: A Playground instance.
+    """
+    pass
---
+++ tests/test_puzzle_ninetytwo.py
@@ -0,0 +1,18 @@
+import unittest
+
+import numpy as np
+
+from arc_puzzle_generator.puzzles.puzzle_ninetytwo import puzzle_ninetytwo
+from arc_puzzle_generator.utils.data_loader import load_puzzle
+from tests.utils import test_dir
+
+
+class PuzzleNinetyTwoTest(unittest.TestCase):
+    def setUp(self):
+        puzzle_path = test_dir / "data" / "16de56c4.json"
+        self.puzzle = load_puzzle(puzzle_path)
+
+    def test_generate_16de56c4(self):
+        playground = puzzle_ninetytwo(self.puzzle.train[0].input)
+        *_, output_grid = playground
+        self.assertTrue(np.array_equal(output_grid, self.puzzle.train[0].output))
