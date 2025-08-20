a10ea8be9af8956db591eebb026204cb8b37d950
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2025-08-20 09:21:17 +0200

    import test data for puzzle 74

---
+++ src/arc_puzzle_generator/puzzles/puzzle_seventyfour.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+from arc_puzzle_generator.playground import Playground
+
+
+def puzzle_seventyfour(input_grid: np.ndarray) -> Playground:
+    """
+    Implements puzzle 74 in which the snake borders the grid and the snake is a single line that does not intersect itself.
+    :param input_grid: The input grid as a 2D NumPy array.
+    :return: The Playground object representing the puzzle.
+    """
+    pass
---
+++ tests/test_puzzle_seventyfour.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+import numpy as np
+
+from arc_puzzle_generator.puzzles.puzzle_seventyfour import puzzle_seventyfour
+from arc_puzzle_generator.utils.data_loader import load_puzzle
+from tests.utils import test_dir
+
+
+class PuzzleSeventyFourTestCase(TestCase):
+    def setUp(self):
+        puzzle_path = test_dir / "data" / "8f3a5a89.json"
+        self.puzzle = load_puzzle(puzzle_path)
+
+    def test_generate_8f3a5a89(self):
+        playground = puzzle_seventyfour(self.puzzle.train[0].input)
+        *_, output_grid = playground
+        self.assertTrue(np.array_equal(output_grid, self.puzzle.train[0].output))
