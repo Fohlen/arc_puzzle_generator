56f1f9aeeca0a3bf5d500987ca26011feecbc740
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2025-08-20 10:31:24 +0200

    Import test data for puzzle 86

---
+++ src/arc_puzzle_generator/puzzles/puzzle_eightysix.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+from arc_puzzle_generator.playground import Playground
+
+
+def puzzle_eightysix(input_grid: np.ndarray) -> Playground:
+    """
+    Puzzle 86 is a puzzle that requires agents to frame polygon shapes.
+    :param input_grid: The input grid for the puzzle, represented as a 2D numpy array.
+    :return: A Playground instance configured for puzzle 86.
+    """
+    pass
---
+++ tests/test_puzzle_eightysix.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+import numpy as np
+
+from arc_puzzle_generator.puzzles.puzzle_eightysix import puzzle_eightysix
+from arc_puzzle_generator.utils.data_loader import load_puzzle
+from tests.utils import test_dir
+
+
+class PuzzleEightySixTestCase(TestCase):
+    def setUp(self):
+        puzzle_path = test_dir / "data" / "aa4ec2a5.json"
+        self.puzzle = load_puzzle(puzzle_path)
+
+    def test_generate_aa4ec2a5(self):
+        playground = puzzle_eightysix(self.puzzle.train[0].input)
+        *_, output_grid = playground
+        self.assertTrue(np.array_equal(output_grid, self.puzzle.train[0].output))
