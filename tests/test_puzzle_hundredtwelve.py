48b286ea214a24bf80ef77fa2278dcd11bdd07fd
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2025-08-22 09:37:46 +0200

    Pass the rest

--- tests/test_puzzle_hundredtwelve.py
+++ tests/test_puzzle_hundredtwelve.py
@@ -13,6 +13,33 @@ class PuzzleHundredTwelveTest(unittest.TestCase):
         self.puzzle = load_puzzle(puzzle_path)
 
     def test_generate_e376de54(self):
-        playground = puzzle_hundredtwelve(self.puzzle.train[0].input)
+        playground = puzzle_hundredtwelve(
+            self.puzzle.train[0].input,
+            orientation="top_right"
+        )
         *_, output_grid = playground
         self.assertTrue(np.array_equal(output_grid, self.puzzle.train[0].output))
+
+    def test_generate_e376de55_second(self):
+        playground = puzzle_hundredtwelve(
+            self.puzzle.train[1].input,
+            orientation="right",
+        )
+        *_, output_grid = playground
+        self.assertTrue(np.array_equal(output_grid, self.puzzle.train[1].output))
+
+    def test_generate_e376de55_third(self):
+        playground = puzzle_hundredtwelve(
+            self.puzzle.train[2].input,
+            orientation="down",
+        )
+        *_, output_grid = playground
+        self.assertTrue(np.array_equal(output_grid, self.puzzle.train[2].output))
+
+    def test_generate_e376de55_prompt(self):
+        playground = puzzle_hundredtwelve(
+            self.puzzle.test[0].input,
+            orientation="bottom_left",
+        )
+        *_, output_grid = playground
+        self.assertTrue(np.array_equal(output_grid, self.puzzle.test[0].output))
